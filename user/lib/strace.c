599119a0acf25b7a7885fbc6f62280f42bebde34
李永琦 <21351023@buaa.edu.cn>
2024-05-22 19:32:34 +0800

    complete lab4-2-exam

--- user/lib/strace.c
+++ user/lib/strace.c
@@ -17,8 +17,22 @@ void strace_send(int sysno) {
 	}
 
 	// Your code here. (1/2)
+	if (straced == 0) return;
+	int straced_bak = straced;
+	straced = 0;
+	ipc_send(env->env_parent_id, sysno, NULL, 0);
+	syscall_set_env_status(env->env_id, ENV_NOT_RUNNABLE);
+	straced = straced_bak;
 }
 
 void strace_recv() {
 	// Your code here. (2/2)
+	int sysno;
+	u_int whom;
+	while ((sysno = ipc_recv(&whom, 0, 0))) {
+		strace_barrier(whom);
+		recv_sysno(whom, sysno);
+		syscall_set_env_status(whom, ENV_RUNNABLE);
+		if (sysno == SYS_env_destroy) break;
+	}
 }
